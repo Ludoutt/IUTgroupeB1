8e10c9e2ebc670f53def1844e1c4621450679db8
Mathieu Piot <math.piot@gmail.com>
2019-03-11 18:31:34 +0100

    Add a Voter

---
+++ src/Security/Voter/BacklogVoter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Backlog;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class BacklogVoter extends Voter
+{
+    protected function supports($attribute, $subject)
+    {
+        return in_array($attribute, ['VIEW', 'EDIT', 'DELETE'])
+            && $subject instanceof Backlog;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case 'VIEW':
+                return true;
+                break;
+            case 'EDIT':
+                return true;
+                break;
+            case 'DELETE':
+                return true;
+                break;
+        }
+
+        return false;
+    }
+}
